1d58f39db7e349fd70a3080776c8c08b3eae374e
Mohammad Rahimi <mashdimamad@gmail.com>
2017-02-20 22:49:52 -0600

    Add files via upload

---
+++ Initial.cpp
@@ -0,0 +1,23 @@
+/* 
+ * File:   Initial.cpp
+ * Author: amin
+ * 
+ * Created on March 3, 2016, 2:49 PM
+ */
+
+#include "Initial.h"
+
+template<class T> 
+Initial<T>::Initial(T a) {
+    
+    std::cout << "FFFFF";
+    nresidue = 1000;
+}
+template<class T>
+Initial<T>::Initial(const Initial& orig) {
+}
+template<class T>
+Initial<T>::~Initial() {
+}
+
+
